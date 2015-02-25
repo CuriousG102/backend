1a3f4f867aea26cdd6367e39a16fa2197de49212
Miles <mileshutson@utexas.edu>
2015-02-25 04:32:21 -0600

    unicode for ze models

--- profsUT/dataCollections/models.py
+++ profsUT/dataCollections/models.py
@@ -10,6 +10,9 @@ class Instructor(models.Model):
     bio = models.TextField(verbose_name='Short biography of the professor', 
                            null=True)
 
+    def __unicode__(self):
+        return self.first + " " + self.last
+
 class Course(models.Model):
     courseName = models.CharField(max_length=50)
     uniqueNo = models.IntegerField(verbose_name="Course Unique Number")
@@ -44,6 +47,9 @@ class Course(models.Model):
     semesterYear = models.IntegerField(max_length=4, choices=YEAR_CHOICES,
                                        default=date.today().year)
 
+    def __unicode__(self):
+        return self.courseName
+
 class CourseTime(models.Model):
     course = models.ForeignKey('Course')
 
@@ -58,12 +64,20 @@ class CourseTime(models.Model):
     time = models.TimeField(verbose_name='Course Begin Time')
     endTime = models.TimeField(verbose_name='Course End Time')
 
+    def __unicode__(self):
+        return "Time for " + self.course.courseName
+
 class Question(models.Model):
     text = models.TextField(verbose_name="Question text")
+
+    def __unicode__(self):
+        return self.text
 
 class Response(models.Model):
     question = models.ForeignKey(Question)
     instructor = models.ForeignKey(Instructor)
     text = models.TextField(verbose_name="Response text")
 
+    def __unicode__(self):
+        return self.instructor + "- " + self.question.text + ": " + self.text
 
