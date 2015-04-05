6affdf6c410b26c6d44cbdc6fb006416ec50e562
Miles <mileshutson@utexas.edu>
2015-04-05 16:47:42 -0500

    bug fix

--- profsUT/profsUT/settings.py
+++ profsUT/profsUT/settings.py
@@ -103,13 +103,14 @@ else:
 
         # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
         # refers directly to STATIC_URL. So it's safest to always set it.
-        STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+        
 
         # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
         # you run `collectstatic`).
         STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
         STATICFILES_LOCATION = 'static'
+        STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
         MEDIAFILES_LOCATION = 'media'
         MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
