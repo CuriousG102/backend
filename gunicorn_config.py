b7415a8e08db67b586cd7c2e65d98eeaa116ee1d
Miles <mileshutson@utexas.edu>
2015-04-04 19:34:10 -0500

    Potential fix to docker problem

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@ import multiprocessing
 
 command = '/usr/local/bin/gunicorn'
 pythonpath = 'opt/site/profsUT'
-bind = '127.0.0.1:8001'
+bind = '0.0.0.0:8001'
 workers = multiprocessing.cpu_count()*2 + 1
