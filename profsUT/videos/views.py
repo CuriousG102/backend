0ec1f46bcd323d9c0bf468c359dc5f6a9852a775
Miles <mileshutson@utexas.edu>
2015-04-12 03:35:28 -0500

    bug fix

--- profsUT/videos/views.py
+++ profsUT/videos/views.py
@@ -6,22 +6,26 @@ from models import Video
 
 import json
 import logging
+import traceback
 
 logger = logging.getLogger(__name__)
 # Create your views here.
 
 @require_POST
 def heyWatchPost(request):
-    response = json.loads(request.body)
-    video = Videos.object.get(transcode_job_id=int(response['id']))
-    video.video_transcoding = False
-    video.transcode_job_id = None
-    if 'errors' in response:
-        logger.error('Error in response for video transcoding job of %s: %s' % (video.instructor, str(response)))
-    else:
-        video.video_thumbnail_url = respone['jpg_640x']
-        video.video_url = response['hls']
-        video.video_transcoded = True
-        logger.info('Success for video transcoding job of %s: %s' % (video.instructor, str(response)))
-    
-    video.save()
+    try:
+        response = json.loads(request.body)
+        video = Videos.object.get(transcode_job_id=int(response['id']))
+        video.video_transcoding = False
+        video.transcode_job_id = None
+        if 'errors' in response:
+            logger.error('Error in response for video transcoding job of %s: %s' % (video.instructor, str(response)))
+        else:
+            video.video_thumbnail_url = respone['jpg_640x']
+            video.video_url = response['hls']
+            video.video_transcoded = True
+            logger.info('Success for video transcoding job of %s: %s' % (video.instructor, str(response)))
+        
+        video.save()
+    except:
+        logger.error('Everything sucks, and your heywatchpost has an uncaught exception: %s' % traceback.format_exc())
