a17c2f5ea0e5aff9c3406710db0211572edf9fdf
Miles <mileshutson@utexas.edu>
2015-04-21 01:04:13 -0500

    DB access optimizations

--- profsUT/dataCollections/views.py
+++ profsUT/dataCollections/views.py
@@ -63,7 +63,7 @@ def thankYou(request):
     })
 
 class InstructorList(generics.ListAPIView):
-	queryset = Instructor.objects.all().prefetch_related('courses').prefetch_related('video')
+	queryset = Instructor.objects.all().prefetch_related('courses')
 	serializer_class = InstructorListSerializer
 
 class InstructorDetail(generics.RetrieveAPIView):
