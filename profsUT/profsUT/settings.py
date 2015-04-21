87f8adf22954346a6ec004c71e84896945c3ed14
Miles <mileshutson@utexas.edu>
2015-04-21 03:05:21 -0500

    Set logging to info level

--- profsUT/profsUT/settings.py
+++ profsUT/profsUT/settings.py
@@ -151,7 +151,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'file': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename': '/var/eb_log/django_log',
         },
@@ -159,7 +159,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
     },
